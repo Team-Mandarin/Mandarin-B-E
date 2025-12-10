b52c9581beb2239374a21091a6d8b5af787aa95f
정민서 <jminseo1832@naver.com>
2025-12-10 17:37:43 +0900

    SimulationMessageRepository.java 오류 해결

--- src/main/java/mandarin/com/mandarin_backend/repository/SimulationMessageRepository.java
+++ src/main/java/mandarin/com/mandarin_backend/repository/SimulationMessageRepository.java
@@ -7,9 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SimulationMessageRepository extends JpaRepository<SimulationMessage, Long> {
-    List<SimulationMessage> findBySimulationSimulationIdOrderByTimestampAsc(Long simulationId);
-}
-
 
     /**
      * 특정 시뮬레이션의 모든 메시지를 시간순으로 조회
